b40c00adc0d112708974d820012a7a69557a2c59
dendnk968 <dendnk968@gmail.com>
2020-05-20 02:24:45 +0300

    solve

--- src/main/java/ru/ifmo/collections/FirstUnique.java
+++ src/main/java/ru/ifmo/collections/FirstUnique.java
@@ -1,19 +1,37 @@
 package ru.ifmo.collections;
 
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.TreeSet;
+
 /**
  * Design a class which contains integers and returns first unique integer (in order of addition).
  * FirstUniqueTest can be used as an example.
  */
 public class FirstUnique {
+    private TreeSet<Integer> uniq = new TreeSet<>();
+    private Set<Integer> multyInt = new HashSet<>();
+
     public FirstUnique(int[] numbers) {
-        // TODO implement
+        for (int number : numbers) {
+            add(number);
+        }
     }
 
     public int showFirstUnique() {
-        throw new UnsupportedOperationException(); // TODO implement
+        if (uniq.size() != 0) {
+            return uniq.first();
+        }
+        return -1;
     }
 
     public void add(int value) {
-        throw new UnsupportedOperationException(); // TODO implement
+        if (uniq.contains(value)) {
+            uniq.remove(value);
+            multyInt.add(value);
+        } else if (!multyInt.contains(value)) {
+            uniq.add(value);
+        }
     }
 }
--- src/main/java/ru/ifmo/collections/KthLargest.java
+++ src/main/java/ru/ifmo/collections/KthLargest.java
@@ -1,5 +1,7 @@
 package ru.ifmo.collections;
 
+import java.util.*;
+
 /**
  * Design a class to find the kth largest element in a stream. k is from 1 to numbers.length.
  * Note that it is the kth largest element in the sorted order, not the kth distinct element.
@@ -8,11 +10,28 @@ package ru.ifmo.collections;
  * For each call to the method KthLargest.add(), return the element representing the kth largest element in the stream.
  */
 public class KthLargest {
+    private int k;
+    private Queue<Integer> s = new PriorityQueue<>(new Comparator<Integer>() {
+        @Override
+        public int compare(Integer o1, Integer o2) {
+            return o2 - o1;
+        }
+    });
     public KthLargest(int k, int[] numbers) {
-        // TODO implement
+        this.k = k;
+        for (int number : numbers) {
+            add(number);
+        }
     }
 
     public int add(int val) {
-        throw new UnsupportedOperationException(); // TODO implement
+        s.add(val);
+        if (s.size() > k){
+            s.poll();
+        }
+        if(s.size() == k && s.size() != 0) {
+            return s.peek();
+        }
+        return -1;
     }
 }
